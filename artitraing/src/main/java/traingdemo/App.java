62790bc407455d101c6f7cc5766b59a39bc822cb
root <root@ip-172-31-35-168.ap-south-1.compute.internal>
2021-08-13 16:25:52 +0000

    updated App

--- artitraing/src/main/java/traingdemo/App.java
+++ artitraing/src/main/java/traingdemo/App.java
@@ -1,13 +1,13 @@
 package traingdemo;
 
 /**
- * Hello world!
+ * Hello India!
  *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello India!" );
     }
 }
