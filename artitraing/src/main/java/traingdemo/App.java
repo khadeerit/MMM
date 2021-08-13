28e1a3d8241c4879327b49ebc9709e2982bbf9ca
root <root@ip-172-31-35-168.ap-south-1.compute.internal>
2021-08-13 07:06:39 +0000

    mvn

---
+++ artitraing/src/main/java/traingdemo/App.java
@@ -0,0 +1,13 @@
+package traingdemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
